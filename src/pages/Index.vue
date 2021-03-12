5a5c2a3e8cbe890760ebbdaaea413bb2ebc58c93
Xinos <userxinos@gmail.com>
2021-03-12 16:12:52 +0700

    update index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,19 +1,36 @@
 <template>
-  <!--  <catalog :data="pages"/>-->
-  <div>Hello world</div>
+  <div class="container">
+
+    <div>Work in Progress!</div>
+    <div class="tag">&lt;/&gt;</div>
+
+    <ul class="pages">
+      <li><h2>Pages</h2></li>
+      <li>
+        <router-link to="modules">Modules</router-link>
+      </li>
+    </ul>
+
+
+    <a
+      href="https://discord.gg/xxzAJjWjs8"
+      target="_blank"
+    >
+      <img
+        class="discord"
+        :src="require('../img/icons/JoinDiscord.png')"
+        alt="discord-button"
+      >
+    </a>
+
+  </div>
 </template>
 
 <script>
 // import {pages} from '../js/pages';
-import Catalog from '../components/Catalog';
+// import Catalog from '../components/Catalog';
 
 export default {
-    // components: {Catalog},
-    // data() {
-    //   return {
-    //     pages,
-    //   };
-    // },
     metaInfo() {
         return {
             title: 'Hades\' Star Data',
@@ -21,3 +38,21 @@ export default {
     },
 };
 </script>
+<style scoped lang="scss">
+.container {
+  margin: 3%;
+  text-align: center;
+  font-size: 140%;
+}
+.tag {
+  font-size: 600%;
+  padding: 3%;
+}
+.pages {
+  list-style-type: none;
+  padding: 5%;
+}
+.discord {
+  width: 20%;
+}
+</style>
