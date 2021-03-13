11cc0b394b59377ffa5c1b2f1c70aea35336d626
Xinos <userxinos@gmail.com>
2021-03-13 10:13:10 +0700

    site stats

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue';
 
-// import {srcollPlugin} from './src/js/plugins';
 import VueProgressBar from 'vue-progressbar';
-// import VueAnalytics from 'vue-analytics';
-// import VueMeta from 'vue-meta';
-// import VueGtag from 'vue-gtag';
+import VueGtag from 'vue-gtag';
 
 import App from './src/components/App.vue';
 import router from './src/js/Vue/router.js';
 
-// import './src/js/directives';
 import './src/css/style.scss';
 
 Vue.config.productionTip = false;
 
-// Vue.use(srcollPlugin, {
-//   router,
-// });
+Vue.use(VueGtag,
+    { config: { id: 'G-BCPM07DZH8' } },
+    router,
+);
+
 Vue.use(VueProgressBar, {
     color: '#345b66',
     failedColor: '#874b4b',
@@ -34,6 +32,5 @@ Vue.use(VueProgressBar, {
 new Vue({
     router,
     render: (h) => h(App),
-})
-    .$mount('#app');
+}).$mount('#app');
 
