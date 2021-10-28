4dc59ddbc822a88f81df2373a48bc82595bebf4b
Xinos <userxinos@gmail.com>
2021-10-28 18:31:07 +0700

    preparing repo for move

--- modules/JsonRaw.js
+++
@@ -1,8 +0,0 @@
-
-/* Основной объект для данных таблицы */
-export default class JsonRaw {
-    constructor(data, metadata = {}) {
-        this.data = data;
-        this.metadata = metadata;
-    }
-}
--- modules/Timer.js
+++
@@ -1,9 +0,0 @@
-export default class {
-    constructor() {
-        this.startTime = new Date().getTime();
-    }
-
-    get final() {
-        return (new Date().getTime() - this.startTime) / 1000;
-    }
-}
